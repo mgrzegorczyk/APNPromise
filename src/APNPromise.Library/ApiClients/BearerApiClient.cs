203278ad5321776e8842f1fe124001f442b53749
mgrzegor <m-grzegorczyk@o2.pl>
2024-06-19 19:24:25 +0200

    bearer api client

---
+++ src/APNPromise.Library/ApiClients/BearerApiClient.cs
@@ -0,0 +1,31 @@
+﻿using APNPromise.Library.ApiClients.Abstractions;
+using APNPromise.Library.Models;
+using APNPromise.Library.Models.Results;
+using Microsoft.Extensions.Logging;
+
+namespace APNPromise.Library.ApiClients
+{
+    public class BearerApiClient : BaseBearerApiClient
+    {
+        public BearerApiClient(HttpClient httpClient, string bearerToken, ILogger<BaseBearerApiClient> logger) 
+            : base(httpClient, bearerToken, logger)
+        {
+        }
+
+        public async Task<IEnumerable<Book>> GetBooksAsync()
+        {
+            return await SendRequestAsync<IEnumerable<Book>>("/api/books");
+        }
+
+        public async Task<PaginatedResult<Order>> GetOrdersAsync(int pageNumber, int pageSize)
+        {
+            var url = $"/api/orders?pageNumber={pageNumber}&pageSize={pageSize}";
+            return await SendRequestAsync<PaginatedResult<Order>>(url);
+        }
+
+        public async Task<HttpResponseMessage> AddBookAsync(Book book)
+        {
+            return await PostRequestAsync("/api/books", book);
+        }
+    }
+}
---
+++ src/APNPromise.Library/Models/Results/PaginatedResult.cs
@@ -0,0 +1,9 @@
+﻿namespace APNPromise.Library.Models.Results;
+
+public class PaginatedResult<T>
+{
+    public IEnumerable<T> Items { get; set; }
+    public int TotalCount { get; set; }
+    public int PageSize { get; set; }
+    public int CurrentPage { get; set; }
+}
